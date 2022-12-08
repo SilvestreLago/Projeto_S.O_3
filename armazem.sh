73546524b22562d5a4a62255fa7572915768ce83
Silvestre <87388202+SilvestreLago@users.noreply.github.com>
2022-12-08 17:46:35 -0300

    Update armazem.sh

--- armazem.sh
+++ armazem.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-echo -n "Digite: 1[Visualizar itens] 2[Cadastrar novos itens]:  "
+echo -n -e O que você deseja fazer? "\n" 1[Visualizar itens] "\n" 2[Cadastrar novos itens] "\n" 3[Remover item] "\n" Digite um valor:" "
 read A1
 
-ES=/home/$USER/estoque.txt
+ES=/home/$USER/estoque
 
 
-if [ ! -e $ES ];then
-        touch $ES
+if [ ! -d $ES ];then
+        mkdir $ES
 fi
 
 case $A1 in
         1)
-                CAT=`cat $ES`
+                CAT=`ls $ES`
                 V=" "
                 if [ $CAT == $V ];then
                         clear
-                        echo "O estoque está vazio."
+                        echo "O estoque está vazio."      
                 else
                         clear
                         echo $CAT
                 fi
                 echo " ";;
         2)
-                echo -n 'Digite o nome do produto, valor e quantidade: '
+                echo -n 'Digite o nome do produto: '      
                 read P1
-                echo "$P1 " >> $ES;;
+                mkdir $ES/$P1
+                echo -n "Digite o valor do produto: R$"   
+                read P2
+                touch $ES/$P1/valor.val
+                echo $P2 >> $ES/$P1/valor.val
+                echo -n "Digite a quantidade do produto: "
+                read P3
+                touch /$ES/$P1/quantidade.qtd
+                echo $P3 >> /$ES/$P1/quantidade.qtd;;     
+        3)
+                echo "Em andamento...";;
         *)
                 echo 'Valor não encontrado.';;
 esac
